abf5ee0f49ab68019bdff0c55ddd29ea643e5a8f
Emerald <emerald.henry@stu.cu.edu.ng>
2023-11-18 01:43:01 +0100

    updates

---
+++ MLOps.py
@@ -0,0 +1,236 @@
+# install azure cli and enable tab completion
+
+#CLI code
+az -- version 
+
+# list all the extensions added
+az extension list
+# installing the ml extension
+az extension add -n ml -y # -n name, ml machine learning, -y yes.
+
+az ml --help # returns a list of the functions that are available in azure ml studio.
+
+az login # helps us login to azure ml from our browser.
+
+az account list -o table # lists all the accounts and subscriptions we have
+
+
+#installing all azureml dependencies and packages on your cli
+cd path\to\directory
+python -m venv env_name
+cd env_name\Scripts
+.\activate.bat   
+
+pip install azureml-core  # installs all the dependencies  
+
+
+# we can create a compute instance and a workspace in azure ml from the command line. Check Azure documentation.
+
+
+# using HuggingFace Transformers for tasks
+#requirements.txt
+ipywidgets==version #required for ipynb
+ipykernel==version  #required for ipynb
+transformers==version #from hugging face transformers
+datasets==version     #from hugging face dataset
+
+#main.py
+!pip install -r requirements.txt
+
+from transformers import pipeline  # check hugging face documentary for more.
+
+ # generator = pipeline('predefined_task', model='model_name')
+generator = pipeline('text-generation', model'gpt2')
+generator("Summarize: input_text") # input your text after the summarize.
+generator("question: input_question")
+
+ 
+
+# Hugging Face Datasets (using the same requirements.txt file as above)
+from datasets import load_dataset, list_datasets
+
+# Explore available datasets
+available = list_datasets()
+print(len(available))
+print([i for i in available if '/' not in i]) # Prints a list of available datasets to choose from
+
+# load the dataset dynamically by passing the name. 
+movie_rationales = load_dataset("movie_rationales")
+# Select the "train" dataset and then port it to pandas
+train = movie_rationales["train"]
+df = train.to_pandas()
+
+
+'''Additional files within an application directory
+
+__init__.py: helps to designate that a directory is a python package
+             for instance if there is an app.py file within the directory named 'package' and
+             there is a function named 'main', we can access that function using 'package.app.main'
+             or from package import __. create one using touch or cat
+    
+requirements.txt: a file that contains all the dependencies needed. install using pip as 'pip install -r requirements.txt'
+
+setup.py: It is relevant for packaging and distribution projects across environments. helps to preserve functionality
+           check below for example script.
+
+.github: Workflows contain actions (CI/CD) and can be created automatically on GitHub or locally through the CLI by writing a .yml script. 
+         Workflows define the actions that will be carried out for CI/CD. They work on GitHub for continuous deployment. 
+         The deployment environment (container registry for docker image) is also configured within workflows for CD. Workflows define the steps to 
+         perform when an action is triggered such as 'push,' is performed.
+
+.vscode: stores customized vscode configuration. it is created automatically when some configurations are changed, it can be created  manually on the CLI.
+
+__pycache__ directory: is a product of python bytecode compilation whenever imports are made. this helps to speed up the process, by checking the cache whenver
+                       the same operation is to be carried out again.
+
+Dockerfile: contains the set of instructions to build a docker image. a docker image (converted from the code directory) is a lightweight package that contains 
+            everything needed to run a software, including code, runtime, dependencies etc. it essentially convert the code dir into a package. called containerization.
+            below is a sample Dockerfile
+
+.gitattribute: helps track large files. It is automatically created when cloning from huggingface. Just use git add .gitattributes to include before pushing back to space.
+
+Test Directory: create an app directory for application, tests directory for tests and other dependencies.
+'''
+
+
+'''Using setup.py for packaging and distributing projects'''
+from setuptools import setup, find_packages
+
+# Read the dependencies from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='your_project_name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=requirements, # we could also load in as install_requires = ['gradio', 'pytorch==3.3']
+    entry_points={
+        'console_scripts': [
+            'your_script_name = your_package.module:main_function', # Jformat = jformat.main:main (jformat.py is the file name, we defined the main function within)
+        ],
+    },
+    # Other metadata such as author, description, license, etc.
+)
+
+
+# after activating your virtual environment and going into the directory
+
+python setup.py develop # it will also install all the dependencies.
+
+
+
+'''an __init__.py file is used to convert a directory to a package and can be empty. below is a sample code'''
+
+# This can be an empty file for simple packages
+
+# You might include package-level initialization code here
+print("Initializing the package!")
+
+# Define variables or constants that should be available to the package's modules
+PACKAGE_NAME = "my_package"
+VERSION = "1.0"
+
+
+'''.gitignore file: create to ignore tracking some directories'''
+touch .gitignore
+# Type text of things not to track. find a detailed .gitignore file with alfredodeza
+
+
+'''creating a sample Dockerfile'''
+touch Dockerfile #creates the file. then within, enter what is below
+
+# Use an official Python runtime as a parent image
+FROM python:3.8-slim
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy the application files to the container
+COPY . .
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# Define environment variable
+ENV NAME World
+
+# Run app.py when the container launches
+CMD ["python", "app.py"]
+
+'''CMD part for docker'''
+docker build -t your-image-name # building the docker image
+
+docker run -p 4000:80 your-image-name #run the docker image locally for testing it (this will provide a url you can checkout). or deploy to cloud (serverless)
+# deploying can be on a server. serverless methods utilize amazon ecs, google kubernetes engine, microsorf azure contaner instance. one of the ffg.
+
+'''IF YOU ARE using CD DEPLOYING USING GITHUB ACTIONS, HERE ARE THE STEPS AFTER CREATING YOUR DOCKER FILE'''
+docker build -t your-image-name  # build the docker image
+
+# Tag your image with the repository url. if it is docker hub, add your username also
+docker tag your-image-name username/your-image-name 
+
+# Push the docker image to the container registry like docker hub.
+docker push username/your-image-name
+
+# deploy the docker image in the registry to you serverless cloud. process differs depending on the cloud service
+
+# Define within github workflows to build the docker image and push to container registry whenever there is an update. push entire code to hub also
+# (eg)
+name: Deploy to Docker Hub
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout Repository
+        uses: actions/checkout@v2
+
+      - name: Set up Docker
+        uses: actions/setup-docker@v2
+
+      - name: Build and Push Docker Image
+        run: |
+          docker build -t username/your-image-name .
+          docker tag username/your-image-name username/your-image-name:latest
+          docker push username/your-image-name:latest
+        env:
+          DOCKER_USERNAME: ${{ secrets.DOCKER_USERNAME }} # use secrets to hide sensitive info
+          DOCKER_PASSWORD: ${{ secrets.DOCKER_PASSWORD }}
+
+# with this, anytime a change is made to the code as specified in workflow, the registry is updated 
+# and it reflects on the deployed software because the software points to the register.
+
+
+'''Typical structure of an application directory'''
+
+myapp/
+|-- app/
+|   |-- __init__.py
+|   |-- module1.py
+|   |-- module2.py
+|   |-- ...
+|-- tests/
+|   |-- __init__.py
+|   |-- test_module1.py
+|   |-- test_module2.py
+|   |-- ...
+|-- requirements.txt
+|-- Dockerfile
+|-- .gitignore
+|-- .github/
+|   |-- workflows/
+|       |-- your-ci-cd-workflow.yml
+|-- .vscode/
+|   |-- settings.json
+|-- setup.py
+|-- ...
---
+++ Python and DSA.py
@@ -0,0 +1,333 @@
+# python
+groceries = [] # this evaluates to false when there isn't any value within
+if groceries:
+    print('done') # doesn't print because it evaluates to false
+
+# in interger is 0, it evaluates to false.
+
+# Exceptions
+
+#raise RuntimeError('error')
+try:
+    results = 14/0
+except ZeroDivisionError: # we want the except to function specific to ZeroDivisionError
+    results = 14/2
+
+# except Exception: Catches all types of exceptions
+except Exception as e;
+    print({str(e)})
+
+# we can also pass multiple exceptions to an except
+except(ZeroDivisionError, TypeError):
+
+# .items(), .keys(), .values() method is specifically used with dictionaries in python
+
+#finding the methods available to an object. List, tuple, dictionary etc
+for method in dir(tuple()): # this could be any other object.
+    if method.startswith('__'):
+        print(method)
+
+
+# lists
+ 
+# insert method
+names = []
+names.append('sugar') # adds sugar to end
+names.insert(1, "sugar") # adds sugar to index 1
+
+shopping = []
+shopping.append(["sugar", 'salt']) # yeilds a nested list
+shopping.extend(["sugar", 'salt']) # yeilds an extended list
+
+shopping.pop(1) # removes from the list using index
+shopping.remove('salt')  # use remove to specify the value
+
+# Dictionaries
+contact = {}
+contact['height'] # height is meant to be the key
+print(contact['height']) # should print the value associated with this key
+
+# using the .get() method will return None if key isn't found.
+result = contact.get('height')
+print(result)
+
+result = contact.get('height', '6ft')
+print(result) # if result is None, the .get method assigns default value '6ft'
+
+result.pop("age") # pops out the value and key 
+
+#functions
+
+# unpacking arguments '*args' converts to a tuple the parameters are positional arguments
+# unpacking '**Kwargs' unpacks to a dictionary enter the parameters as keyword arguments to store
+
+#classes
+
+class cars(): # basic class
+    pass
+
+car = cars() # instantiating the class
+
+dir(car) # lists all the default methods associated with this class object
+
+class car ():
+    def __init__(self, brand):
+        print(f"brand is {brand}") # parameters can be used directly to perform operations
+        self.car_brand = brand  # variables however must be associated to the class as a class attribute
+    def count(self, val):  # class methods must be on the same line level as the __init__ and must be associated with the class
+        print(self.car_brand) # within the __init__ method, we can print brand but without, we'll have to assign it to a class attribute 'self.car_brand'
+        counting = val # within a normal function, we can assign without referring to the class
+        return val # used within the function and never without.
+    def repeat(self, num):
+        recursion = self.count(num) # using a function within a function requires referring to the class
+
+
+# Generally, if you want a variable to be reusable outside the class method it was defined, define it as a class attribute using the 'self.name' = value
+#NB: we can use a function defined above in one defined below
+
+def expense(self, amount):
+    'do something'
+    self.report() #report is used here but defined below
+def report(self):
+    print(f"some things")
+
+
+#inheritance
+class pet:
+    def eat(self):
+        self.food = self.food - self.appetite # we didn't define the class attributes, we just defined the operation
+        print(self.food) 
+
+class dog(pet):
+    def __init__(self, food, appetite):
+        self.food = food                 # we defined the class attribute after inheritance
+        self.appetite = appetite
+
+#print all the methods of a class
+dogs = dog()
+
+for attribute in dir(dogs):
+    if attribute.startswith("__"):
+        continue
+    print(attribute)  # printing all attribute except those starting with '__'
+
+
+# python modules and packages
+pwd # prints the working directory
+touch pandas.py # creates a file
+mkdir pack 
+cd pack 
+cat __init__.py
+cat module.py
+
+
+# import modules
+import pandas
+from pandas import x
+
+# from packages (the __init__.py file tells python that this directory is a package)
+import pack.module # importing a module within a package
+
+pack.module.func() # using a function from the module within a package
+
+# another way is to import modules within the __init__.py file. that way, when the package is imported, modules automatically become available
+
+
+import sys
+
+def main(arguments):
+    print("hooked")
+    for argument in arguments:
+        print(argument)
+
+if __name__ == '__main__': # checks if script is being run as a main program and not imported as a module
+    main(sys.argv) #prints 'hooked' the 'file_name' then all comand line arguments passed to in on the command line.
+
+
+# virtual environments in python (useful for installing dependencies) the following are command line or power shell code.
+
+python3 -m venv myenv # venv is a new environment
+tree myenv #lists all python packages and modules in this environment
+which python # shows the directory of the python and includes the virtual environment if within.
+# activating environment for cmd
+myenv\Scritps\activate   
+# activating environment for powershell
+.\myenv\Scripts\Activate
+
+echo 'flask' >requirements.txt # writes 'flask' into the requirement.txt file
+
+pip install -r requirement.txt # installing all in the requirement.txt file
+
+deactivate # used to deactivate the virtual environment hence all installed or imported won't be available anymore.
+
+
+# Testing (writing and executing them)
+cd preferred_directory #
+cd - # takes you to the previous directory.
+
+%%writefile addition.py 
+def add(a,b):
+    return(a + b)
+def sub(a, b):
+    return(a - b)
+
+%%writefile test_addition.py 
+pip install pytest
+import addition
+import pytest 
+def test_add():
+    assert addition.add(4, 5) == 9
+def test_sub():
+    assert addition.sub(4, 5) == -1
+
+#running the test_addition.py file should show two passed or 1 passed and one failed.
+
+# Using Unitest
+import unittest
+class TestExm(unitest.TestCase): # Interit the base class'unitest.TestCase'
+    def test_assertion(self):
+        self.assertEquals('some string', 'others') #checking equals. you can add functions to this method
+        # we can also use self.assertNotAlmostEqual
+
+unitest.main(argv=[''], verbosity = 2, exit = False)
+
+# fails because 'some string' != 'others'
+# NB: name your test directory with 'tests' so as not to collide with inbuilt dirs
+# test files and test functions should be prefixed with 'test_'
+# test classes should be prefixed with captial "T" eg 'TestClass'
+# test directories are like packages and will require the '__init__.py' file
+
+#cmd or powershell
+python -m venv pytest # creating a virtual environment named pytest
+
+echo pytest >requirements.txt # write pytest into the requiremts.txt file
+pip install -r requirements.txt
+
+cd test_directory
+pytest # by calling pytest in the test directory, the test is ran.
+# for pytest to run the file, it has to abide by the test naming convention.
+
+# if this is the test file that gets ran, notice that it follows the conventions and has no other thing inside except test functions.
+def test_add():
+    assert addition.add(4, 5) == 9
+def test_sub():
+    assert addition.sub(4, 5) == -1
+
+# if there are other statements that aren't test functions, use pytest filename.py to run the test
+# use pytest -vvv filename.py to see the entire script when running your code
+
+# Test Classes
+class TestStrToInt:
+    def setup(self):
+        print('\n this is setup')
+    def teardown(self):
+        print('\nthis is teardown')
+    def setup_class(cls):
+        print('\nthis is setup class')
+    def teardown_class(cls):
+        print('\nthis is teardown class')
+    def test_rounds_down(self):  # all above are setup. this is the first test
+        result = str_to_int('1.99') #str_to_int is the function that converts str to int. I didn't define.
+        assert result == 1   
+    def test_round_down_lesser(self): # second test.
+        result = str_to_int('2.2')
+        assert result == 2
+
+
+# parameterizing test 
+# we have a function 'str_to_bool' that converts 'yes', 'y' or '' to True.
+
+#normal method
+def test_str_yes():
+    result = str_to_bool('yes')
+    assert result == True
+
+def test_str_y():
+    result = str_to_bool('y')
+    assert result == True
+
+    ...
+
+# using parameterization, we use pytest decorator to wrap extra capabilities
+
+@pytest.mark.parametrize('value', ['y', 'yes','']) # making this iterable
+def test_is_true(value):
+    result = str_to_bool(value)
+    assert result is True
+
+# output 'F..' means 1 failure and 2 passing
+
+
+# seting a debugger to better understand the cmd test output
+pytest --pdb  -v test_file.py # on command line. -v is verbosity. you can use -vvv to increase this
+
+# enter 'h' to see what command line functions you have to better explain the failure, use 'l' to list the failure line in code
+# to see all the methods available to you, use 'pytest --help'
+
+
+
+# fixtures help avoid doing the def setup method by automatically figuring out the setup and avoiding test file path collitions
+# remember, all files outputed from command line are saved to a location.
+class TestFixtures:
+  def test_write_yes(self, tmpdir): # specifying where the test output will be saved to
+    path = str(tmpdir.join('test_value')) # joining the test_value str to the path
+    write_integer('Yes', path) # this function writes the str 'yes' to the path. The function also uses the 'str_to_bool' function
+    with open(path, r) as _f: 
+      value = _f.read()
+    assert value == 'True'
+
+
+# Pandas 
+
+# Activating python virtual environment on cmd
+cd path\to\your\directory
+python -m venv env_name
+cd env_name\Scripts
+activate
+
+# on powershell
+cd path\to\your\directory
+python -m venv env_name
+cd env_name\Scripts
+.\activate.bat #.\ is a powershell read convention
+
+# on bash
+cd path\to\your\directory
+python -m venv env_name
+env_name/bin/activate
+
+# Pandas
+# you do not need to download the file from the internet, just pass the url and pandas will read
+file_name = 'https://github.com/henrii1/wind-turbine-power-curve-modelling/raw/main/dataset/kelmarsh_02.xlsx'
+
+import pandas as pd
+read = pd.read_excel(file_name)
+read.head(10)
+
+# Converting the tables to markdown files for teaching etc
+!pip install tabulate #required dependency
+from pandas.io.clipboard import to_clipboard # i want the converted file to save to clipboard
+md = read.to_markdown() #writing to markdown
+to_clipboard(md, excel=True) #using 'to_clipboard', i want the file to paste as an excel table
+
+
+#use queries with pandas
+df.query('ratings > 30').head(10) # put all forms of queries into the brace. .head is for display
+pab = df.query("region.str.contains('robles', na = False)", engine = 'python') # because string contains functions, you'll have to specify the engine
+
+#changes to string
+df['var_short'] = df['variety'].replace({'red wine': 'r', 'white wine': 'w'}) #replace red wine with r and white wine with w, store in var_short
+
+#applying functions in pandas
+def good_wine(value):
+  if value > 94:
+    return True
+  return False
+
+df['good'] = df['rating'].apply(good_wine) # Adds a new column called good, if ratings is greater than 94, it appends true.
+
+
+
+
+
+
